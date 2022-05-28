fbbb78c43669c8020e4917fef32a239cc9b39c5f
Anna <k_anna_i@mail.ru>
2022-05-28 21:11:02 +0300

    correction

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,27 +1,31 @@
 import { useState } from 'react'
 
 export default function Converter () {
-  const [color, setColor] = useState('')
+  const [rgb, setRGB] = useState('')
+  const [color, setColor] = useState('#c0fffe')
 
   const handler = ({target}) => {
-    if (target.value > 7) {
+    if (target.value.length !== 7) {
       return
     }
 
     const input =  /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(target.value)
     if (input === null) {
-      setColor('Error')
+      setRGB('Error')
       return
     }
 
-    const rgb = `${parseInt(input[1], 16)}, ${parseInt(input[2], 16)}, ${parseInt(input[3], 16)}`
-    setColor(`rgb${rgb}`)
+    const convert = `${parseInt(input[1], 16)}, ${parseInt(input[2], 16)}, ${parseInt(input[3], 16)}`
+    setRGB(`rgb(${convert})`)
+    setColor(input[0])
   }
 
   return(
-    <form className="convertet" style={{backgroundColor: color}}>
-      <input className="input" onChange={handler} />
-      <div className="result">{color}</div>
+    <form className="convertet">
+      <div className="container" style={{backgroundColor: color}}>
+        <input className="input" placeholder="Введите #hex" pattern='^#[0-9A-Fa-f{6}]' onChange={handler} />
+        <div className="result">{rgb}</div>
+      </div>
     </form>
   )
 }
